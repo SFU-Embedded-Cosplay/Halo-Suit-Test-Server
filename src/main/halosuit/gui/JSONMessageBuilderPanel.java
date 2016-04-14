a33542a2bfead755b3e228037d4c8ae783cb801c
Tyler Rubenuik <trubenui@sfu.ca>
2016-04-13 23:35:01 -0700

    Add JSON parsing that creates GUI elements
    This is the first step towards the JsonMessageBuilder module

--- src/main/halosuit/gui/JSONMessageBuilderPanel.java
+++ src/main/halosuit/gui/JSONMessageBuilderPanel.java
@@ -1,13 +1,69 @@
 package main.halosuit.gui;
 
+import java.awt.GridLayout;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.lang.reflect.Type;
+import java.util.List;
+
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonIOException;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonSyntaxException;import com.google.gson.reflect.TypeToken;
+
+
 public class JSONMessageBuilderPanel extends JPanel {
+	
+	JsonObject json = new JsonObject();
 	
 	JButton addButton = new JButton("Add");
 	
-	public JSONMessageBuilderPanel() {
+	public JSONMessageBuilderPanel() {	
+		
+		setLayout(new GridLayout(10, 3, 10, 10));
+		
+		
+        json = getJsonObjectFromFile("res//PossibleJson.json");
+        
+       json.entrySet().forEach( entry -> {
+    	   
+    	   System.out.println(entry.getKey());
+    	   System.out.println(entry.getValue());
+    	   
+    	   
+    	   if(entry.getValue().isJsonArray()) {
+    		   
+    		   Type listType = new TypeToken<List<String>>() {}.getType();
+    		   List<String> yourList = new Gson().fromJson(entry.getValue(), listType);
+    		   
+    		   
+    		   add(new MessageBuilderItem(entry.getKey(), yourList));
+    	   }
+    	   
+    	   
+       });
+
+		
+		
+		
 		add(addButton);
+	}
+	
+	private JsonObject getJsonObjectFromFile(String fileName) {
+		JsonParser parser = new JsonParser();
+        JsonElement jsonElement = null;
+        
+		try {
+			jsonElement = parser.parse(new FileReader("res//PossibleJson.json"));
+		} catch (JsonIOException | JsonSyntaxException | FileNotFoundException e) {
+			e.printStackTrace(); // TODO: handle this properly
+		}
+		
+		return jsonElement.getAsJsonObject();
 	}
 }
---
+++ src/main/halosuit/gui/MessageBuilderItem.java
@@ -0,0 +1,24 @@
+package main.halosuit.gui;
+
+import java.util.List;
+
+import javax.swing.ButtonGroup;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+
+public class MessageBuilderItem extends JPanel{
+			
+	public MessageBuilderItem(String key, List<String> options) {
+		add(new JLabel(key));
+		
+		ButtonGroup optionGroup = new ButtonGroup();
+		for(String option : options) {
+			JRadioButton optionRadioButton = new JRadioButton(option);
+			
+			optionGroup.add(optionRadioButton);
+			
+			add(optionRadioButton);
+		}				
+	}
+}
