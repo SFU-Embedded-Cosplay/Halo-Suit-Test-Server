982431d6b455f8505cdffb8abe5192df6b4b5d7f
Tyler Rubenuik <trubenui@sfu.ca>
2016-04-17 02:48:14 -0700

    Add test for MessageBuilderPanel

--- src/test/java/halosuit/gui/messagebuilder/MessageBuilderPanelTest.java
+++ src/test/java/halosuit/gui/messagebuilder/MessageBuilderPanelTest.java
@@ -1,15 +1,20 @@
 package test.java.halosuit.gui.messagebuilder;
 
 import static org.junit.Assert.*;
+import main.java.halosuit.gui.MessageBuilder.MessageBuilderPanel;
 
 import org.junit.Test;
 
 public class MessageBuilderPanelTest {
-
+	
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGetMessage() {
+		String jsonFilePath = "src\\test\\resource\\testJson1.json";
+		
+		MessageBuilderPanel panel = new MessageBuilderPanel(jsonFilePath, (message) -> {});
+		
+		assertEquals("{}", panel.getMessage());
 	}
 
 }
