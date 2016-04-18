9b1795831b7c3b40d43995c8dbe6871c683e1b80
Tyler Rubenuik <trubenui@sfu.ca>
2016-04-18 03:17:22 -0700

    Improve visual layout of MessageBuilderSwitch items

--- src/main/java/halosuit/gui/MessageBuilder/MessageBuilderSwitch.java
+++ src/main/java/halosuit/gui/MessageBuilder/MessageBuilderSwitch.java
@@ -1,5 +1,7 @@
 package main.java.halosuit.gui.MessageBuilder;
 
+import java.awt.Color;
+import java.awt.Component;
 import java.awt.GridLayout;
 import java.util.Enumeration;
 
@@ -9,29 +11,43 @@ import javax.swing.ButtonModel;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
+import javax.swing.SpringLayout;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 
 public class MessageBuilderSwitch extends JPanel implements MessageBuilderItem {
 			
+	public static final int KEY_LEFT_PADDING = 5;
+	public static final int ELEMENT_PADDING = 5;
+	
 	private JLabel key = null;
 	private ButtonGroup optionGroup = new ButtonGroup();
 	
 	public MessageBuilderSwitch(String key, String[] options) {
 		this.key = new JLabel(key);
 		
-		setLayout(new GridLayout());
+		SpringLayout layout = new SpringLayout();
+		
+		layout.putConstraint(SpringLayout.WEST, this.key, KEY_LEFT_PADDING, SpringLayout.WEST, this);
 		
 		add(this.key);
+		
+		Component lastComponet = this.key;
 		
 		for(String option : options) {
 			JRadioButton optionRadioButton = new JRadioButton(option);
 			optionRadioButton.setActionCommand(option);
 			
+			layout.putConstraint(SpringLayout.WEST, optionRadioButton, ELEMENT_PADDING, SpringLayout.EAST, lastComponet);
+			lastComponet = optionRadioButton;
+			
 			optionGroup.add(optionRadioButton);
 			add(optionRadioButton);
 		}				
+
+		setLayout(layout);
+
 	}
 
 	@Override
