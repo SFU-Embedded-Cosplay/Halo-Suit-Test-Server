5fb35ddac511f73ce31a4ec175f7813b52d2734a
Tyler Rubenuik <trubenui@sfu.ca>
2016-04-18 00:09:59 -0700

    Add more tests for building messages

--- src/test/java/halosuit/gui/messagebuilder/MessageBuilderPanelTest.java
+++ src/test/java/halosuit/gui/messagebuilder/MessageBuilderPanelTest.java
@@ -15,6 +15,18 @@ public class MessageBuilderPanelTest {
 		MessageBuilderPanel panel = new MessageBuilderPanel(jsonFilePath, (message) -> {});
 		
 		assertEquals("{}", panel.getMessage());
+			
+		panel.setValue("lights", "off");
+		assertEquals("{\"lights\":\"off\"}", panel.getMessage());
+
+		panel.setValue("heart rate", "50700");
+		assertEquals("{\"lights\":\"off\",\"heart rate\":50700}", panel.getMessage());
+		
+		panel.setValue("water temperature", "5.7265");
+		assertEquals("{\"lights\":\"off\",\"water temperature\":5.7265,\"heart rate\":50700}", panel.getMessage());
+		
+		panel.setValue("voltage2", "hello world 531");
+		assertEquals("{\"lights\":\"off\",\"water temperature\":5.7265,\"heart rate\":50700,\"voltage2\":\"hello world 531\"}", panel.getMessage());
 	}
 
 }
